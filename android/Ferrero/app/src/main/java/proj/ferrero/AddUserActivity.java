6650bd5e37e7ea07796eed143a7a3e6a740f18d1
Tonny Quintos <tonnyquintos@Tonnys-MacBook-Pro.local>
2016-01-17 22:40:16 +0800

    added fields for add user

--- android/Ferrero/app/src/main/java/proj/ferrero/AddUserActivity.java
+++ android/Ferrero/app/src/main/java/proj/ferrero/AddUserActivity.java
@@ -1,16 +1,61 @@
 package proj.ferrero;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.Date;
+
+import proj.ferrero.models.User;
 
 public class AddUserActivity extends AppCompatActivity {
+
+    EditText etName, etTag, etAge, etBday, etBlood
+            , etAllergy, etMedCond, etContactPerson, etContactNumber;
+    Button btnSave;
+
+    SqlDatabaseHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_user);
+
+        btnSave = (Button) findViewById(R.id.btn_save);
+        etName = (EditText) findViewById(R.id.et_name);
+        etTag = (EditText) findViewById(R.id.et_tag);
+        etBday = (EditText) findViewById(R.id.et_bday);
+        etAge = (EditText) findViewById(R.id.et_age);
+        etBlood = (EditText) findViewById(R.id.et_bloodtype);
+        etAllergy = (EditText) findViewById(R.id.et_allergies);
+        etMedCond = (EditText) findViewById(R.id.et_med_conditions);
+        etContactPerson = (EditText) findViewById(R.id.et_contact_person);
+        etContactNumber = (EditText) findViewById(R.id.et_contact_number);
+
+        dbHelper = new SqlDatabaseHelper(this);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                long bday = (new Date(etBday.getText().toString())).getTime();
+
+                dbHelper.createUser(new User(etTag.getText().toString(),
+                        etName.getText().toString(),
+                        1000,
+                        Integer.parseInt(etAge.getText().toString()),
+                        bday,
+                                etBlood.getText().toString(),
+                                etAllergy.getText().toString(),
+                                etMedCond.getText().toString(),
+                                etContactPerson.getText().toString(),
+                                etContactNumber.getText().toString()));
+            }
+        });
     }
 
     @Override
